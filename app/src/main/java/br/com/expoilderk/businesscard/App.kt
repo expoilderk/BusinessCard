f3fc13efb036b65261c7d64b87353cdc83740384
expoilderk <falecommarlonreis@gmail.com>
2021-10-26 20:30:07 -0300

    Configura o Room Database no projeto

--- app/src/main/java/br/com/expoilderk/businesscard/App.kt
+++ app/src/main/java/br/com/expoilderk/businesscard/App.kt
@@ -1,7 +1,10 @@
 package br.com.expoilderk.businesscard
 
 import android.app.Application
+import br.com.expoilderk.businesscard.data.AppDataBase
+import br.com.expoilderk.businesscard.data.BusinessCardRepository
 
 class App : Application() {
-
+    val dataBase by lazy { AppDataBase.getDataBase(this) }
+    val repository by lazy { BusinessCardRepository(dataBase.businessDao()) }
 }
