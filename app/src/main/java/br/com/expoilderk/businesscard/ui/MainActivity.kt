d5a5def6322427577b161785c8901a9fd8038717
expoilderk <falecommarlonreis@gmail.com>
2021-10-26 22:00:05 -0300

    Cria Adapter com listagem de cartões

--- app/src/main/java/br/com/expoilderk/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/expoilderk/businesscard/ui/MainActivity.kt
@@ -2,16 +2,25 @@ package br.com.expoilderk.businesscard.ui
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import br.com.expoilderk.businesscard.App
 import br.com.expoilderk.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBisunessCard()
         insertListener()
     }
 
@@ -20,5 +29,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddBusinessCardActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun getAllBisunessCard() {
+        mainViewModel.getAll().observe(this, { businessCards ->
+            adapter.submitList(businessCards)
+        })
     }
 }
